e0318ea2be628564e94ab6b00ced5b937bbc4b76
Franco Sinisi <franco@hyperganic.com>
2018-07-16 17:22:19 -0300

    Sockets config.

--- config/env/production.js
+++ config/env/production.js
@@ -237,10 +237,9 @@ module.exports = {
 		 * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
 		 *                                                                          *
 		 ***************************************************************************/
-		// onlyAllowOrigins: [
-		//   'https://example.com',
-		//   'https://staging.example.com',
-		// ],
+		onlyAllowOrigins: [
+		   'https://test-sails-franco.herokuapp.com',
+		 ],
 
 		/***************************************************************************
 		 *                                                                          *
