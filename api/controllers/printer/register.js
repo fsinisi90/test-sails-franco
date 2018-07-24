460c10e2d4babda450dda3cc24ea6e42424a4b35
Franco Sinisi <franco@hyperganic.com>
2018-07-24 15:29:26 -0300

    no message

--- api/controllers/printer/register.js
+++ api/controllers/printer/register.js
@@ -1,57 +1,51 @@
 module.exports = {
 
-  friendlyName: 'Register printer',
+    friendlyName: 'Register printer',
 
-  description: 'Register a printer in Cloud Factory through WebSockets.',
+    description: 'Register a printer in Cloud Factory through WebSockets.',
 
-  inputs: {
-    id: {
-      type: 'number'
+    inputs: {
+        token: {
+            type: 'string'
+        }
+    },
+
+    fn: async function (inputs, exits) {
+        let ws = new sails.WebSocket(sails.config.custom.cloudFactoryWS);
+        let _this = this;
+
+        ws.on('open', function open() {
+            console.log('WebSocket connection opened.');
+
+            let subscribe = {
+                command: 'subscribe',
+                identifier: JSON.stringify({
+                    channel: 'CloudFactoryChannel',
+                }),
+            };
+
+            ws.send(JSON.stringify(subscribe));
+        });
+
+        ws.on('message', function incoming(data) {
+            console.log(data);
+            let message = JSON.parse(data);
+            if (message.type == 'welcome') {
+                let register = {
+                    command: 'message',
+                    identifier: JSON.stringify({
+                        channel: 'CloudFactoryChannel',
+                    }),
+                    data: JSON.stringify({
+                        action: 'register',
+                        email: _this.req.me.emailAddress,
+                        token: inputs.token
+                    }),
+                };
+
+                ws.send(JSON.stringify(register));
+            }
+        });
     }
-  },
-
-  fn: async function (inputs, exits) {
-    const ws = new sails.WebSocket('wss://techlab-staging-pr-275.herokuapp.com/cable');
-    let printer = await Printer.findOne(inputs.id);
-
-    ws.on('open', function open() {
-      console.log('ws connection open');
-       
-      let msg = {
-        command: 'subscribe',
-        identifier: JSON.stringify({
-        channel: 'CloudFactory::ServicesChannel',
-        }),
-      };
-
-      ws.send(JSON.stringify(msg));
-      
-      setTimeout(function() {
-
-        let msg2 = {
-          command: 'message',
-          identifier: JSON.stringify({
-          channel: 'CloudFactory::ServicesChannel',
-          }),
-          data: JSON.stringify({
-          action: 'register',
-          printer_name: printer.name,
-          printer_model: printer.model,
-          factory_name: 'Factory1'
-          }),
-        };
-        
-        ws.send(JSON.stringify(msg2));
-        
-      }, 500);
-    });
-
-    ws.on('message', function incoming(data) {
-      console.log(data);
-    });
-
-    return 1;
-  }
-
 
 };
